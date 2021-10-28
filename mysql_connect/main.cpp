e4e995bd48c63e3b7f963b229cf2d97d92124e59
CHO HYE RA <s2037@e-mirim.hs.kr>
2021-10-28 15:54:05 +0900

    로그인 했을 때 발생되는 오류 수정

--- mysql_connect/main.cpp
+++ mysql_connect/main.cpp
@@ -241,9 +241,17 @@ void login() {
 				cout << "로그인 되었습니다.";
 
 			}
-			else cout << "아이디 또는 비밀번호가 일치하지 않습니다.";
+			else {
+				cout << "아이디 또는 비밀번호가 일치하지 않습니다.";
+				Sleep(1000);
+				system("cls");
+				login();
+			}
+			} else {
+			cout << "아이디 또는 비밀번호가 일치하지 않습니다";
+			
+		}
 
-		} else cout << "아이디 또는 비밀번호가 일치하지 않습니다";
 		Sleep(1000);
 		system("cls");
 		foodMarket();
